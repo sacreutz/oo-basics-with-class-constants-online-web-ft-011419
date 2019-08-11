3578c3f12af063d4e0cc6c1adc7d98e66389aa26
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-11 17:29:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if !BRANDS.contains(brand)
-      BRANDS << brand 
+   # if !BRANDS.contains(brand)
+    BRANDS << brand 
   end
 
   def cobble
